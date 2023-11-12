6773cd5de0a97cd56b93d0a0257d88913caeb6ab
Larissa Magalhães <teamlemonsolucoes@gmail.com>
2023-11-12 14:05:36 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import "./app.scss";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Searchbar from "./components/searchbar/Searchbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Gigs from "./pages/gigs/Gigs";
+import Gig from "./pages/gig/Gig";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Add from "./pages/add/Add";
+import Orders from "./pages/orders/Orders";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import MyGigs from "./pages/myGigs/MyGigs";
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+
+
+  const App = () => {
+    const handleSearch = (query) => {
+      console.log(`Realizando a pesquisa: ${query}`);
+      // Adicione a lógica de pesquisa aqui
+    };
+  
+    return (
+      <div className="app">
+        <h1>Meu Aplicativo</h1>
+        <Searchbar onSearch={handleSearch} />
+      </div>
+    );
+  };
+  
+
+
+
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/gigs",
+          element: <Home />,
+        },
+        {
+          path: "/",
+          element: <Gigs />,
+        },
+        {
+          path: "/myGigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+      ],
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Áreas de interesse:</h2>
+            <span>Realidade Virtual</span>
+            <span>Design Gráfico</span>
+            <span>Narrativas</span>
+            <span>Cinema & Audiovisual</span>
+            <span>Música & Som</span>
+            <span>Programação</span>
+            <span>Animação</span>
+          </div>
+          <div className="item">
+            <h2>Sobre nós</h2>
+            <span>Politíca de Privacidade</span>
+            <span>Termos de Uso do Serviço</span>
+            <span>Contato</span>
+          </div>
+          <div className="item">
+            <h2>Support</h2>
+            <span>Help & Support</span>
+            <span>Trust & Safety</span>
+    
+          </div>
+          <div className="item">
+            <h2>Community</h2>
+            <span>Customer Success Stories</span>
+            <span>Community hub</span>
+            <span>Forum</span>
+            <span>Events</span>
+            <span>Blog</span>
+            <span>Influencers</span>
+            <span>Affiliates</span>
+            <span>Podcast</span>
+            <span>Invite a Friend</span>
+            <span>Become a Seller</span>
+            <span>Community Standards</span>
+          </div>
+          <div className="item">
+            <h2>More From matcch</h2>
+            <span>Business</span>
+            <span> Pro</span>
+            <span>Logo Maker</span>
+            <span>Guides</span>
+            <span>Get Inspired</span>
+            <span> Select</span>
+            <span>ClearVoice</span>
+            <span>Workspace</span>
+            <span>Learn</span>
+            <span>Working Not Working</span>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>matcch.</h2>
+            <span>© Matcch. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="/img/twitter.png" alt="" />
+              <img src="/img/facebook.png" alt="" />
+              <img src="/img/linkedin.png" alt="" />
+              <img src="/img/instagram.png" alt="" />
+            </div>
+            
+        
+            <img src="/img/accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
