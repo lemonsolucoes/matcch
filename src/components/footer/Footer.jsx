3e8d7e4b5eecff583b4f82a51ee9dce120eb1894
Larissa Magalhaes <larisscmagalhaes@gmail.com>
2023-11-27 20:10:33 -0300

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,50 +6,8 @@ function Footer() {
     <div className="footer">
       <div className="container">
         <div className="top">
-          <div className="item">
-            <h2>Áreas de interesse:</h2>
-            <span>Realidade Virtual</span>
-            <span>Design Gráfico</span>
-            <span>Narrativas</span>
-            <span>Cinema & Audiovisual</span>
-            <span>Música & Som</span>
-            <span>Programação</span>
-            <span>Animação</span>
-          </div>
-          <div className="item">
-            <h2>Sobre nós</h2>
-            <span>Politíca de Privacidade</span>
-            <span>Termos de Uso do Serviço</span>
-            <span>Contato</span>
-          </div>
-          <div className="item">
-            <h2>Suportet</h2>
-            <span>Help & Support</span>
-            <span>Trust & Safety</span>
-    
-          </div>
-          <div className="item">
-            <h2>Teste</h2>
-            <span>Customer Success Stories</span>
-            <span>Community hub</span>
-            <span>Forum</span>
-            <span>Events</span>
-            <span>Blog</span>
-            <span>Invite a Friend</span>
-            <span>Become a Seller</span>
-            <span>Community Standards</span>
-          </div>
-          <div className="item">
-            <h2>Mais sobre matcch</h2>
-            <span>Business</span>
-            <span> Pro</span>
-            <span>Logo Maker</span>
-            <span>Guides</span>
-            <span>Get Inspired</span>
-            <span> Select</span>
-            <span>Learn</span>
-            <span>Working Not Working</span>
-          </div>
+
+         
         </div>
         <hr />
         <div className="bottom">
