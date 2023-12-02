e8cdef2ffc3ecbee8db0e2cc5d87498bd0aee50f
Fatimaleh <fatimalete67@gmail.com>
2023-12-02 12:09:20 -0300

    Adicionando queries sql para a table de requests

---
+++ matcch_api/src/database/repositories/request.js
@@ -0,0 +1,42 @@
+import db from "#database/client.js";
+
+export async function querySelectAllRequestsBySender(data) {
+  const { senderId } = data;
+
+  const [rows] = await db.query(
+    `
+    SELECT * FROM REQUEST
+    WHERE sender_id = ?
+    ORDER BY created_at DESC;
+	`,
+    [senderId],
+  );
+  return rows;
+}
+
+export async function queryInsertRequest(data) {
+  const { senderId, recipientId } = data;
+
+  const [rows] = await db.query(
+    `
+		INSERT INTO REQUEST (status, sender_id, recipient_id)
+		VALUES ('pending',?,?);
+	`,
+    [senderId, recipientId],
+  );
+  return rows;
+}
+
+export async function queryUpdateRequest(data) {
+  const { status, id } = data;
+
+  const [rows] = await db.query(
+    `
+		UPDATE REQUEST
+    SET status = ?
+		WHERE id = ?;
+	`,
+    [status, id],
+  );
+  return rows;
+}
