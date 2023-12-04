7c05d69a204255c7aee6ac9da9293b32e220921a
lemonsolucoes <147517012+lemonsolucoes@users.noreply.github.com>
2023-12-04 15:38:45 +0000

    Update 3 files

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -28,18 +28,51 @@ const Orders = () => {
             <div className="header">
               <img src={gig.pp} alt="Imagem" />
               <h3>Solicitação enviada para {gig.username}</h3>
-              <label>1° opção</label>
             </div>
+            <label>1° opção</label>
             <div className="subtitle">Gerenciar solicitação</div>
             <div className="buttons-container">
               <button className="circular-button">
                 <i className="fa-sharp fa-solid fa-user"></i>
+                <p>Perfil</p>
               </button>
               <button className="circular-button">
                 <i className="fa-sharp fa-solid fa-pen"></i>
+                <p>Editar</p>
               </button>
               <button className="circular-button">
                 <i className="fa-sharp fa-solid fa-trash"></i>
+                <p>Deletar</p>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="box">
+          <h4>
+            <i className="fa-sharp fa-solid fa-circle-xmark"></i> Solicitações
+            Recusadas
+          </h4>
+
+          <div className="card">
+            <div className="header">
+              <img src={gig.pp} alt="Imagem" />
+              <h3>Solicitação enviada para {gig.username}</h3>
+            </div>
+            <label>1° opção</label>
+            <div className="subtitle">Gerenciar solicitação</div>
+            <div className="buttons-container">
+              <button className="circular-button">
+                <i className="fa-sharp fa-solid fa-user"></i>
+                <p>Perfil</p>
+              </button>
+              <button className="circular-button">
+                <i className="fa-sharp fa-solid fa-pen"></i>
+                <p>Editar</p>
+              </button>
+              <button className="circular-button">
+                <i className="fa-sharp fa-solid fa-trash"></i>
+                <p>Deletar</p>
               </button>
             </div>
           </div>
